8de711cf74a53ad3b0e5936b5635f831a153e9ba
thinkabovelimit <kkrishnaprasad.mec@gmail.com>
2020-09-07 17:32:12 +0530

    Comments added

--- src/main/java/CsvtoJson.java
+++ src/main/java/CsvtoJson.java
@@ -3,15 +3,18 @@
 import pack.*;  //package pack contains a method convertion to convert csv to json
 
 import java.io.IOException;
+import java.util.*;
 
-public class CsvtoJson {
+public class CsvtoJson{
 
     public static void main(String args[]) throws IOException {
-        Csvconvertion csvc = new Csvconvertion();    //create a method of convertion.
 
-//        int[] arr=csvc.Filereader();  //Function to read from the config file
+
+        Csvconvertion csvc = new Csvconvertion();    //create an object of class convertion.
+
         csvc.readfile();
-        csvc.convertion();    //Function for file convertion.
+        List list=csvc.convertion();    //Function for file convertion.
+        csvc.Writefile(list);
 
 
     }
--- src/main/java/pack/Csvconvertion.java
+++ src/main/java/pack/Csvconvertion.java
@@ -12,12 +12,18 @@ import packages for file operation
 import java.io.*;
 import java.util.*;
 import java.io.IOException;
-abstract class read{
+abstract class Read_Write{
+    /*
+    Variables Inputfile and outputfile stores files for input and output
+    Indexes are used to specify the from and to values to be printed and full values or selected values to be printed.
+     */
     String Inputfile;
     String Outputfile;
     int index1;
     int index2;
     int index3;
+
+    //This method reads all the values like inputfile ,outputfile indexes etc
     public void readfile() {
         FileReader reader = null;
         try {
@@ -34,17 +40,21 @@ abstract class read{
         Inputfile = p.getProperty("input");
         Outputfile = p.getProperty("output");
         index1 = Integer.parseInt(p.getProperty("index1"));
-         index2 = Integer.parseInt(p.getProperty("index2"));
-         index3 = Integer.parseInt(p.getProperty("index3"));
+        index2 = Integer.parseInt(p.getProperty("index2"));
+        index3 = Integer.parseInt(p.getProperty("index3"));
     }
-    abstract public void convertion() throws IOException;
+
+
+    abstract  List convertion() throws IOException;
+
+     abstract void Writefile(List list) throws IOException;
 
 
 
 
 
 }
-public class Csvconvertion extends read{
+public class Csvconvertion extends Read_Write{
 /*
 class Csvconverter has two functions convertion and Filereader convertion is used in the process of convertion of csv to json.
 Filereader is used to read from and to values to be printed ie( json format values to be printed).
@@ -53,49 +63,56 @@ Filereader is used to read from and to values to be printed ie( json format valu
 
 
     @Override
-    public void convertion () throws IOException {
+    public List convertion () throws IOException {
 
 
         File input = new File(Inputfile); //File input is given as str
 
-                CsvSchema csv = CsvSchema.emptySchema().withUseHeader(true);//Create an object of csvschema this tells the schema of the file withUserheader means take the first value of column as header
-                CsvMapper csvMapper = new CsvMapper();    //create an object of csvmapper to read an input file.
-                MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader(Map.class).with(csv).readValues(input);  //Mappingiterator is used to map key value pairs
-                    //with specifies the schema to which read
+        CsvSchema csv = CsvSchema.emptySchema().withUseHeader(true);//Create an object of csvschema this tells the schema of the file withUserheader means take the first value of column as header
+
+        CsvMapper csvMapper = new CsvMapper();    //create an object of csvmapper to read an input file.
+
+        MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader(Map.class).with(csv).readValues(input);  //Mappingiterator is used to map key value pairs
+        //with specifies the schema to which read
 
         List<Map<?, ?>> list = mappingIterator.readAll();  //mapping iterator returns list containing the list of json objects
-                if (index3 == 1){     //If input from config file is 1 print values from index1 to index2 as specified in the file
-                    FileWriter myWriter = new FileWriter(Outputfile);
-
-                    try {
-                        for (int i = index1; i <= index2; i++)
-
-                            //System.out.println(list.get(i));
-                            myWriter.write(String.valueOf(list.get(i)));
-                        myWriter.close();
-                    } catch (Exception e) {
-                        System.out.println("Error occured while parsing the file check the indexes");
-
-                    }
-
-
-                }
-                else {          //Print full values
-                    //System.out.println(list);
-
-                    FileWriter myWriter = new FileWriter(Outputfile);
-                    myWriter.write(String.valueOf(list));
-                    myWriter.close();
-                }
-
-
-
-
-
-        }
+        return list;
 
 
 
     }
+    /*
+    Writefile method is used to write the json data into a file.
+     */
+    @Override
+    public void Writefile(List list) throws IOException {
+            if (index3 == 1) {     //If input from config file is 1 print values from index1 to index2 as specified in the file
+                FileWriter myWriter = new FileWriter(Outputfile);
+
+                try {
+                    for (int i = index1; i <= index2; i++)
+
+                        //System.out.println(list.get(i));
+                        myWriter.write(String.valueOf(list.get(i)));
+                    myWriter.close();
+                } catch (Exception e) {
+                    System.out.println("Error occured while parsing the file check the indexes");
+
+                }
+
+
+            } else {          //Print full values
+                //System.out.println(list);
+
+                FileWriter myWriter = new FileWriter(Outputfile);
+                myWriter.write(String.valueOf(list));
+                myWriter.close();
+            }
+
+        }
+    }
+
+
+
     
 
